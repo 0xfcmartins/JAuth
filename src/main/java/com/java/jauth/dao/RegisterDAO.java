e89b539bd0df5ffffc79acd1f650731125af8370
francisco.drive.7@gmail.com <CFCdfg78-->
2022-03-19 22:54:30 +0000

    Add oracle configuration

--- src/main/java/com/java/jauth/dao/RegisterDAO.java
+++ src/main/java/com/java/jauth/dao/RegisterDAO.java
@@ -6,12 +6,30 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
 @Component
 @Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class RegisterDAO {
 
+    @Resource
+    DataSource dataSource;
+
+    public void SomeComponent(DataSource dataSource) throws SQLException {
+        System.out.println("Database connection valid =" + dataSource.getConnection().isValid(1000));
+    }
+
     public RegisterResponseDTO register(RegisterDTO request) {
         //"Do the database insert"
+        String sql = "SELECT * FROM WEB_APP_USR";
+
+        try {
+            SomeComponent(dataSource);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
         return new RegisterResponseDTO(1, "User " + request.username + ", registered with success!");
     }
